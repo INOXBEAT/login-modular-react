da13eafbde93fbd53db97f705b6a8ae9f05b10e9
Javier Preciado <jpreciado85@gmail.com>
2025-01-18 23:24:36 -0500

    modificación de app.css y app.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';  // Importar el archivo CSS
+import LoginForm from './components/LoginForm';  // Importar el componente LoginForm
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
